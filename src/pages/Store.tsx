e71e3df2712e8296305eac0dcb16652b5e5903f9
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-03 16:56:42 -0600

    show items on the store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItems } from "../components/StoreItems";
 
 export const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <>
+      <h1>Store</h1>
+
+      <Row md={2} xs={1} lg={3} className="gap-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItems {...item} />
+          </Col>
+        ))}
+        <Col></Col>
+      </Row>
+    </>
+  );
+};
