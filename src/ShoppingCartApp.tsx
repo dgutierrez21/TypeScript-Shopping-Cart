740adbb9cbddbe27aac8cb2867035e066ada3b11
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-03 18:27:28 -0600

    create a shopping cart context

--- src/ShoppingCartApp.tsx
+++ src/ShoppingCartApp.tsx
@@ -2,14 +2,15 @@ import { Container } from "react-bootstrap";
 import { NavBar } from "./components/NavBar";
 
 import { Outlet } from "react-router-dom";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 export const ShoppingCartApp = () => {
   return (
-    <>
+    <ShoppingCartProvider>
       <NavBar />
       <Container className="mb-4">
         <Outlet />
       </Container>
-    </>
+    </ShoppingCartProvider>
   );
 };
