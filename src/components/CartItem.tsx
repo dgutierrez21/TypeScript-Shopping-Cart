74f18dd2f9aa668d356b92f8e3368186f5411e20
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-05 07:16:33 -0600

    show quantity of each item, price and total | add button to remove from cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/SCContext";
 import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type CartItemProps = {
   id: number;
@@ -15,11 +16,36 @@ export const CartItem = ({ id, quantity }: CartItemProps) => {
   if (item == null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
+
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              {quantity}x
+            </span>
+          )}
+        </div>
+
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+
+      <div>{formatCurrency(item.price * quantity)}</div>
+
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
     </Stack>
   );
 };
