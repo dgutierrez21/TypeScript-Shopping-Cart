b36867295d77f91edb4bc558efcd56a2e834020a
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-04 21:39:52 -0600

    show images of the items in the shopping cart

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/SCContext";
+import storeItems from "../data/items.json";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+export const CartItem = ({ id, quantity }: CartItemProps) => {
+  const { removeFromCart } = useShoppingCart();
+
+  const item = storeItems.find((i) => i.id === id);
+
+  if (item == null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2}>
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+    </Stack>
+  );
+};
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,13 +1,21 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/SCContext";
+import { CartItem } from "./CartItem";
 
 export const ShoppingCart = () => {
-  const { closeCart, isOpen } = useShoppingCart();
+  const { closeCart, isOpen, cartItems } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 };
